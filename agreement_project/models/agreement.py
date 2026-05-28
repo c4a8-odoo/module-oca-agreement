b1bf710eafa4ad7b3209b62661c03741388b2f7d
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2026-05-28 12:31:24 +0200

    fixup! [MIG] agreement_project: Migration to 19.0

--- agreement_project/models/agreement.py
+++ agreement_project/models/agreement.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Agreement(models.Model):
@@ -14,9 +14,13 @@ class Agreement(models.Model):
         string="Projects",
     )
 
+    @api.depends("project_ids.tasks.agreement_id")
     def _compute_task_count(self):
+        groups = self.env["project.task"]._read_group(
+            [("agreement_id", "in", self.ids)],
+            groupby=["agreement_id"],
+            aggregates=["__count"],
+        )
+        counts = {agreement.id: count for agreement, count in groups}
         for ag in self:
-            count = self.env["project.task"].search_count(
-                [("agreement_id", "=", ag.id)]
-            )
-            ag.task_count = count
+            ag.task_count = counts.get(ag.id, 0)
--- agreement_project/tests/test_agreement.py
+++ agreement_project/tests/test_agreement.py
@@ -7,24 +7,14 @@ class TestAgreement(TransactionCase):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.agreement = cls.env["agreement"].create(
-            {"code": "AGR-001", "name": "Agreement 1"}
-        )
-        cls.agreement2 = cls.env["agreement"].create(
-            {"code": "AGR-002", "name": "Agreement 2"}
+            {"code": "AGR-001", "name": "Agreement"}
         )
         cls.project = cls.env["project.project"].create(
-            {"name": "Test Project", "agreement_id": cls.agreement2.id}
+            {"name": "Test Project", "agreement_id": cls.agreement.id}
         )
         cls.task = cls.env["project.task"].create(
             {"name": "Test Task", "project_id": cls.project.id}
         )
 
-    def test_task_inherits_agreement_from_project(self):
-        self.assertEqual(self.task.agreement_id, self.agreement2)
-
     def test_agreement_task_count(self):
-        self.agreement2._compute_task_count()
-        self.assertEqual(self.agreement2.task_count, 1)
-
-    def test_agreement_project_ids(self):
-        self.assertIn(self.project, self.agreement2.project_ids)
+        self.assertEqual(self.agreement.task_count, 1)
