OCA/agreement

[17.0][MIG] agreement_legal: Migration to 17.0

agreement_legal/static/src/js/list_view.esm.js
@@ -1,8 +1,8 @@
 /** @odoo-module **/
 
-import {registry} from "@web/core/registry";
 import {ListController} from "@web/views/list/list_controller";
 import {listView} from "@web/views/list/list_view";
+import {registry} from "@web/core/registry";
 import {useService} from "@web/core/utils/hooks";
 
 export class AgreementListController extends ListController {
@@ -19,7 +19,7 @@ export class AgreementListController extends ListController {
 export const AgreementListView = {
     ...listView,
     Controller: AgreementListController,
-    buttonTemplate: "agreement.ListView.Buttons",
+    buttonTemplate: "agreement.ListView",
 };
 
 registry.category("views").add("agreement_template_tree", AgreementListView);

c4a8-odoo/module-oca-agreement

agreement_legal/static/src/js/list_view.esm.js
@@ -1,8 +1,8 @@
 /** @odoo-module **/
 
-import {registry} from "@web/core/registry";
 import {ListController} from "@web/views/list/list_controller";
 import {listView} from "@web/views/list/list_view";
+import {registry} from "@web/core/registry";
 import {useService} from "@web/core/utils/hooks";
 
 export class AgreementListController extends ListController {
@@ -19,7 +19,7 @@ export class AgreementListController extends ListController {
 export const AgreementListView = {
     ...listView,
     Controller: AgreementListController,
-    buttonTemplate: "agreement.ListView.Buttons",
+    buttonTemplate: "agreement_legal.ListView",
 };
 
 registry.category("views").add("agreement_template_tree", AgreementListView);